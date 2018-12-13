bb7af5cbb29b178ecc2fde2f7b776dbc1a76c491
zhangyunqing666 <839259612@qq.com>
2018-12-13 15:52:54 +0800

    test

--- src/Game.java
+++ src/Game.java
@@ -22,6 +22,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+//zwkswez
 public class Game extends JFrame {
     private static final long serialVersionUID = 1L;
     private static Background backgroundpanel;
