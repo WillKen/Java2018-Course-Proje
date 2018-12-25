856fa26589169e2edd8909de9fe9dab0d4986ca7
zhangyunqing666 <839259612@qq.com>
2018-12-25 22:22:59 +0800

    加了shop类和palyer类

---
+++ src/Shop.java
@@ -0,0 +1,130 @@
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import static java.lang.Thread.sleep;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+
+public class Shop extends JFrame{
+	private player Player = player.getInstance();
+	private static final long serialVersionUID = 1L;
+    private static Background backgroundpanel;
+    private static JButton buy_hp;
+    private static JButton buy_weapon_1;
+    private static JButton buy_weapon_2;
+    private static JLabel message;
+    public Shop()
+    {
+    	message = new JLabel();
+        message.setHorizontalAlignment(SwingConstants.CENTER);
+        message.setForeground(Color.blue);
+        message.setText(" ");
+        message.setFont(new Font("宋体", Font.PLAIN, 24));
+        message.setBounds(20, 15, 1000, 24);
+
+    	backgroundpanel= new Background();
+        backgroundpanel.setImage(new ImageIcon(getClass().getResource("logo.png")).getImage());// 设置背景图片
+        backgroundpanel.add(message);
+        getContentPane().add(backgroundpanel, BorderLayout.CENTER);
+        setBounds(100, 100, 1600, 900);
+    	buy_hp = new JButton("购买HP药剂：100$");
+    	buy_hp.setFont(new Font("宋体", Font.PLAIN, 32));
+    	buy_hp.setForeground(Color.BLUE);
+    	buy_hp.setName("start");
+    	buy_hp.setVisible(true);
+    	buy_hp.setSize(500,60);
+    	buy_hp.setLocation(690, 600);
+    	buy_hp.addMouseListener(new MenuMouseListener_HP());
+        backgroundpanel.add(buy_hp);
+        
+        buy_weapon_1 = new JButton("购买屠龙宝刀：1000$");
+        buy_weapon_1.setFont(new Font("宋体", Font.PLAIN, 32));
+        buy_weapon_1.setForeground(Color.BLUE);
+        buy_weapon_1.setName("start");
+        buy_weapon_1.setVisible(true);
+        buy_weapon_1.setSize(500,60);
+        buy_weapon_1.setLocation(690, 680);
+        buy_weapon_1.addMouseListener(new MenuMouseListener_W1());
+        backgroundpanel.add(buy_weapon_1);
+        
+        buy_weapon_2 = new JButton("购买尚方宝剑：1000$");
+        buy_weapon_2.setFont(new Font("宋体", Font.PLAIN, 32));
+        buy_weapon_2.setForeground(Color.BLUE);
+        buy_weapon_2.setName("start");
+        buy_weapon_2.setVisible(true);
+        buy_weapon_2.setSize(500,60);
+        buy_weapon_2.setLocation(690, 760);
+        buy_weapon_2.addMouseListener(new MenuMouseListener_W2());
+        backgroundpanel.add(buy_weapon_2);    
+    }
+    private class MenuMouseListener_HP extends MouseAdapter{
+    	public void mousePressed(final MouseEvent e) {
+    		if(e.getButton()==e.BUTTON1){
+    			if(Player.get_money()>=100)
+    			{
+    				Player.addhp(1);
+    				Player.addmoney(-100);
+    				message.setText("你购买了HP药剂，HP+1，现在你还剩："+Player.get_money()+"$.");
+    			}
+    			else
+    			{
+    				message.setText("你没有足够的钱！");
+    			}
+    		}
+    	}	
+    }
+    private class MenuMouseListener_W1 extends MouseAdapter{
+    	public void mousePressed(final MouseEvent e) {
+    		if(e.getButton()==e.BUTTON1){
+    			if(Player.get_money()>=1000)
+    			{
+    				Player.addatk(1);
+    				Player.addmoney(-1000);
+    				message.setText("你购买了屠龙宝刀，ATK+1，现在你还剩："+Player.get_money()+"$.");
+    			}
+    			else
+    			{
+    				message.setText("你没有足够的钱！");
+    			}
+    		}
+    	}	
+    }
+    private class MenuMouseListener_W2 extends MouseAdapter{
+    	public void mousePressed(final MouseEvent e) {
+    		if(e.getButton()==e.BUTTON1){
+    			if(Player.get_money()>=1000)
+    			{
+    				Player.addatk(2);
+    				Player.addmoney(-1000);
+    				message.setText("你购买了尚方宝剑，ATK+2，现在你还剩："+Player.get_money()+"$.");
+    			}
+    			else
+    			{
+    				message.setText("你没有足够的钱！");
+    			}
+    		}
+    	}	
+    }
+    public static void main(String[] args) {
+    	Shop game = new Shop();
+        game.setVisible(true);
+    }
+}
---
+++ src/player.java
@@ -0,0 +1,63 @@
+
+public class player {
+	private static player instance = null;
+	private int score;
+	private int money;
+	private int attack;
+	private int hp;
+	public player()
+	{
+		this.score = 0;
+		this.money = 100000000;
+		this.attack = 0;
+		this.hp = 4;
+	}
+/*public player(int score_,int money_,int attack_,int hp_)
+	{
+		this.score = score_;
+		this.money = money_;
+		this.attack = attack_;
+		this.hp = hp_;
+	}*/
+	public static synchronized player getInstance()
+	{
+		if(instance==null)
+			instance = new player();
+		return instance;
+	}
+	
+	public int get_attack()
+	{
+		return this.attack;
+	}
+	public int get_hp()
+	{
+		return this.hp;
+	}
+	public int get_money()
+	{
+		return this.money;
+	}
+	public int get_score()
+	{
+		return this.score;
+	}
+	public void addscore(int score_)
+	{
+		this.score+=score_;
+	}
+	public void addatk(int atk)
+	{
+		this.attack+=atk;
+	}
+	public void addhp(int hp)
+	{
+		this.hp+=hp;
+	}
+	public void addmoney(int money_)
+	{
+		this.money+=money_;
+	}
+	
+	
+}
