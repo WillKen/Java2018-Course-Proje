23b8d13240de27a8a1c8cfa466e9cc93327ea1c3
zhangyunqing666 <839259612@qq.com>
2019-01-16 10:43:45 +0800

    加入关卡

---
+++ src/ShopThread.java
@@ -0,0 +1,32 @@
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import static java.lang.Thread.sleep;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+
+public class ShopThread extends Thread{
+    private static JButton nextLevel;
+	private Shop shop = Shop.getInstance();
+	public void run(){
+		shop.setVisible(true);
+	}
+}
